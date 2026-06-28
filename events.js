90c8b8197b1d593746f9a2f39f769410ae4dbf40
jeyavarshini03 <jeyavarshini003@gmail.com>
2026-06-28 11:01:12 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,29 @@
+function changeColor(){
+    document.getElementById("heading").style.color = "blue";
+}
+
+function showMessage(){
+    alert("Button Clicked Successfully");
+}
+
+function displayText(){
+    let text =
+    document.getElementById("textbox").value;
+
+    document.getElementById("output").innerHTML =
+    "You Typed: " + text;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
